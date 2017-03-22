d15fb562e2f005e47433994ed4154e7f69ba904f
Tom Haycock-West <tomhaycockwest@gmail.com>
2017-03-22 16:19:54 +0000

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,19 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=d32646e3462feb34520186b1e719ed77&units=metric';
+
+module.exports = {
+	getTemp: function(location) {
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+	  return axios.get(requestUrl).then(function(res) {
+			if (res.data.cod === 200){
+				return res.data.main.temp
+      }
+			throw res.data.cod;
+		}, function(res) {
+			throw (res && ((res.response && res.response.data && (res.response.data.message || res.response.data)) || (res.code))) || res;
+		});
+	}
+}
